506ef11923f7b2141aa8da484a09ca3d45257fea
demo <skboardgames@gmail.com>
2025-07-08 15:19:46 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en" className="scroll-smooth">
-			<body className="antialiased mx-auto my-20 container px-4">
+			<body className="antialiased">
 				{children}
 			</body>
 		</html>
